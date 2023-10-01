d62c7debcf1cd2a267283718c5cdde09a9d14f6a
かいちょう <76948385+kaicyou2532@users.noreply.github.com>
2023-10-02 07:29:26 +0900

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,1 +1,16 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    
+    $data = "Name: " . $name . "\nEmail: " . $email . "\nMessage: " . $message;
+    
+    $file = fopen("data.txt", "a");
+    fwrite($file, $data . "\n\n");
+    fclose($file);
+    
+    echo "Data saved successfully.";
+}
+?>
 
